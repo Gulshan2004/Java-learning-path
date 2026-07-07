ff94e20ca74ec1e2535c65ff579193742df66734
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-08 00:19:18 +0530

    Getters and Setters

---
+++ 01-Core-Java/OOP/GetterSetter.java
@@ -0,0 +1,21 @@
+//When ever we want to get the value of a variable we use the getVariable_name method  this is called getter
+//when ever we want to set the value of a variable we use the setVariable_name method this is called setter
+class Human{
+    private int age; //default 0
+    private String name; // default Null
+
+    public int getAge(){ //Getter
+        return age;
+    }
+    public void setAge(int a){//Setter
+        age = a;
+    }
+    
+    public String getName(){//Getter
+        return name;
+    }
+    public void setName(String b){//Setter
+        name = b;
+    }
+}
+//NOTE: The Abopve code only demonstrates the use of getter and setter , it dosent contain the main method.
