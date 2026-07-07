f6b1585ed65b4342a677f1228accc48a81724b4b
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-08 00:09:22 +0530

    Concept of Encapsulation

--- 01-Core-Java/OOP/Encapsulation.java
+++ 01-Core-Java/OOP/Encapsulation.java
@@ -1,6 +1,6 @@
 class Human{
-    private int age;
-    private String name;
+    private int age; //default 0
+    private String name; // default Null
 
     public int getAge(){
         return age;
@@ -8,12 +8,22 @@ class Human{
     public String getName(){
         return name;
     }
+    public void setAge(int a){
+        age = a;
+    }
+    public void setName(String b){
+        name = b;
+    }
 }
 public class Encapsulation {
     public static void main(String args[]){
         Human obj =new Human();
+        obj.setAge(34);
+        obj.setName("Gulshan");
         //obj.age =11;
         //obj.name ="Gulshan";
         System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+        System.out.println(obj.getName() + ":" + obj.getAge());
     }
 }
