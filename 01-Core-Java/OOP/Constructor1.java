140f05a3777a4c9c4054bd1d9ce1b25247f70d45
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-08 13:49:05 +0530

    Concept of Constructor

--- 01-Core-Java/OOP/Constructor1.java
+++ 01-Core-Java/OOP/Constructor1.java
@@ -1,29 +1,31 @@
 class Human{
     private int age;
     private String name; 
-
-    public Human(){ // Constructor It is a Special Method with the same name as the class name and do not have a return type.
+    //Constructor  is a Special Method with the same name as the class name and do not have a return type.
+    public Human(){ // Default  Constructor
         age = 12;
         name = "John";
     }
+    public Human(int age, String name){ //Parameterized Constructor
+        this.age =age;
+        this.name =name;
+    }
+
     public int getAge(){
         return age;
     }
+    
     public String getName(){
         return name;
-    }
-    public void setAge(int age){
-        this.age = age;
-    }
-    public void setName(String name){
-        this.name = name;
     }
 }
 public class Constructor1{
     public static void main(String args[]){
         Human obj =new Human();
         // Every time we create an object we call the constructor 
+        Human obj1 = new Human(32, "Gulshan");
         System.out.println(obj.getName() + ":" + obj.getAge());
+        System.out.println(obj1.getName() + ":" + obj1.getAge());
         //obj.setAge(34);
         //obj.setName("Gulshan");
         //System.out.println(obj.getName() + ":" + obj.getAge());
---
+++ 01-Core-Java/OOP/Default_and_parameterized_constructor.java
@@ -0,0 +1,3 @@
+public class Default_and_parameterized_constructor{
+    
+}
