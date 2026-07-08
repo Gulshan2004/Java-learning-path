04dfcf633fd8b39096cbb12fc381b7d7ada5140d
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-08 13:24:22 +0530

    Constructor

---
+++ 01-Core-Java/OOP/Constructor1.java
@@ -0,0 +1,32 @@
+class Human{
+    private int age;
+    private String name; 
+
+    public Human(){ // Constructor It is a Special Method with the same name as the class name and do not have a return type.
+        age = 12;
+        name = "John";
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
+public class Constructor1{
+    public static void main(String args[]){
+        Human obj =new Human();
+       // Human obj1 =new Human(); // Every time we create an object we call the constructor 
+        System.out.println(obj.getName() + ":" + obj.getAge());
+        //obj.setAge(34);
+        //obj.setName("Gulshan");
+        //System.out.println(obj.getName() + ":" + obj.getAge());
+    }
+}
+
