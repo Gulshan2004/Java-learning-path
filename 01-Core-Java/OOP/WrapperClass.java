1423f629ef027e5034dfb01f10aa1eb461e77e0a
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-10 23:50:55 +0530

    Concept of Wrapper class

---
+++ 01-Core-Java/OOP/WrapperClass.java
@@ -0,0 +1,17 @@
+public class WrapperClass {
+    public static void main(String args[]){
+        int num =7;
+       // Integer num1 = new Integer(num);//boxing (getting a value from primitive type to object type) [MANUAL]
+        Integer num1 = num; //auto-boxing 
+
+        //int num2 =num1.intValue(); // unboxing ( getting a value from object type to a primitive type ) [MANUAL]
+        int num2 =num1; //auto-unboxing
+        System.out.println(num2);
+
+        String str = "12";
+        int num3= Integer.parseInt(str);
+        System.out.println(num3*2);
+    }
+}
+
+//for Every Primitive type in java we have a class for it .
