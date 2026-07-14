0288f989a2c0ee205ea5de652db267073de7cf3a
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-14 22:15:26 +0530

    Concept of Multilevel Inheritance

--- Core-Java/OOP/MultiLevelInheritance.java
+++ Core-Java/OOP/MultiLevelInheritance.java
@@ -1,4 +1,4 @@
-class Calc{
+class Calc3{
     public int add (int n1,int n2){
         return n1+n2;
     }
@@ -7,7 +7,7 @@ class Calc{
     }
 }
 
-class AdvCalc extends Calc{ //We use the extends keyword to inherit the property of base clase i.e Calc into dervived class i.e AdvCalc.
+class AdvCalc3 extends Calc3{ //We use the extends keyword to inherit the property of base clase i.e Calc into dervived class i.e AdvCalc.
     public int multi(int n1,int n2){
         return n1*n2;
     }
@@ -16,7 +16,7 @@ class AdvCalc extends Calc{ //We use the extends keyword to inherit the property
     }
 }
 
-class ScifiCal extends AdvCalc{ // This is an Example of Multi Level Inheritance where AdvCalc extends Calc and ScifiCal Extends AdvCalc.
+class ScifiCal extends AdvCalc3{ // This is an Example of Multi Level Inheritance where AdvCalc extends Calc and ScifiCal Extends AdvCalc.
     public double power(int n1, int n2){
         return Math.pow(n1, n2);
     }
@@ -24,8 +24,8 @@ class ScifiCal extends AdvCalc{ // This is an Example of Multi Level Inheritance
 
 public class MultiLevelInheritance {
     public static void main(String args[]){
-        // calc obj =new  calc();
-        // AdvCalc obj =new  AdvCalc();
+        // calc3 obj =new  calc3();
+        // AdvCalc3 obj =new  AdvCalc3();
         ScifiCal obj = new ScifiCal();
         int r1 =obj.add(2,3);
         int r2 =obj.sub(7,3);
