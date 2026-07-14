008ccb84baef20cd68ebff6ce41389015ee35835
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-14 22:13:24 +0530

    Concept of Method Overriding in java

--- Core-Java/OOP/MethodOverriding.java
+++ Core-Java/OOP/MethodOverriding.java
@@ -1,17 +1,17 @@
-class Calc{
+class Calc1{
     public int add(int n1 , int n2){
         return n1+ n2;
     }
 
 }
-class AdvCalc extends Calc{
+class AdvCalc1 extends Calc1{
     public int add(int n1 , int n2){ // This is Called Method Overriding where add() of AdvCalc class overrids the add() of Calc class
         return n1 + n2+ 1;
     }
  }
 public class MethodOverriding {
     public static void main(String args[]){
-        AdvCalc obj =new AdvCalc();
+        AdvCalc1 obj =new AdvCalc1();
         int r1 =obj.add(2,4);
         System.out.println(r1);
     
