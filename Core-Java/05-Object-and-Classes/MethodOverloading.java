c4c32baf413e02a35c0813257ae4738d69450f14
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-14 22:18:16 +0530

    Concept of MethodOverloading in Java

--- Core-Java/05-Object-and-Classes/MethodOverloading.java
+++ Core-Java/05-Object-and-Classes/MethodOverloading.java
@@ -1,4 +1,4 @@
-class Calculator{
+class Calculator0{
     public int add(int n1, int n2,int n3){
         return n1+ n2+ n3;
     }
@@ -11,7 +11,7 @@ class Calculator{
 }
 public class MethodOverloading {
     public static void main(String args[]){
-        Calculator obj = new Calculator();
+        Calculator0 obj = new Calculator0();
         int r1= obj.add(2,4,9);
         System.out.println(r1);
     }
