15ec880ce97f2c28ad6b63f4ee56b06596a8809c
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-15 21:26:43 +0530

    Quiz_Console_App

---
+++ Mini-Projects/quiz_console_app/QuestionService.java
@@ -0,0 +1,8 @@
+public class QuestionService {
+    Question[] questions = new Question[5];
+    
+    public void displayQuestions(){
+        System.out.println(questions[0].getQuestion());
+    }
+   
+}
