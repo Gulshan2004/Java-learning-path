d32797e314f653d608709d25a538ec1f7c9eccbd
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-16 17:10:48 +0530

    Quiz Console App

--- Mini-Projects/quiz_console_app/QuestionService.java
+++ Mini-Projects/quiz_console_app/QuestionService.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.util.Scanner; //Imports the Scanner class so that we can take input from the keyboard.
 
 public class QuestionService {
-    Question[] questions = new Question[5]; // Array of Questions
-    String selection[] = new String[5]; // Array of 5 values to store user input
+    Question[] questions = new Question[5]; // Array that stores   Question Object with name=questions
+    String selection[] = new String[5]; //This stores the answers entered by the user.
 
     public QuestionService() {
         questions[0] = new Question(1, "size of int", "2", "6", "4", "8", "4");
@@ -14,33 +14,33 @@ public class QuestionService {
 
     public void playQuiz() {
         int i = 0;
-        for (Question q : questions) {
-            System.out.println("Question no:. : " + q.getId());
+        for (Question q : questions) { //Take each Question object from questions[] and store it inside q
+            System.out.println("Question no. : " + q.getId());
             System.out.println(q.getQuestion());
             System.out.println(q.getOpt1());
             System.out.println(q.getOpt2());
             System.out.println(q.getOpt3());
             System.out.println(q.getOpt4());
             Scanner sc = new Scanner(System.in); // Helps get the value from th user
-            selection[i] = sc.nextLine();
-            i++;
+            selection[i] = sc.nextLine();// Suppose User enters 4 then selection[0]="4" , It bassically stores the value entered by the user in a sequential manner 
+            i++;//Move to the Next position 
         }
-        for (String s : selection) {
+        for (String s : selection) { // This prints the Entire Selection Array after the loop ends whuch contains the value entered by the user 
             System.out.println(s);
         }
     }
-    public void printScore(){
-        int score=0;
+    public void printScore(){ //Checks whether answers are correct.
+        int score=0; //initally 0 
         for(int i=0;i<questions.length;i++){
-            Question que = questions[i]; //
+            Question que = questions[i]; //Current question 
             String actualAnswer = que.getAnswer();// this will check the actual answer
-            String userAnswer =selection[i];
+            String userAnswer =selection[i];// this will check the user's Answer 
 
-            if(actualAnswer.equals(userAnswer)){
-                score ++;
+            if(actualAnswer.equals(userAnswer)){ // compares both the answers
+                score ++; // if true than increment score
             }
         }
-        System.out.println("Your score is : " + score);
+        System.out.println("Your score is : " + score); // After the loop ends enter the final score
     }
 
 }
