48f1e7b41037f484efdce34840b89d03efbf0657
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-19 18:21:21 +0530

    Try ewith multiple Catch

---
+++ Java-Fundamentals/Advance_Java/Exception-Handling/TryAndMultipleCatch.java
@@ -0,0 +1,28 @@
+public class TryAndMultipleCatch {
+    public static void main(String args[]) {
+        int i = 2;
+        int j = 0;
+        int nums[] = new int[5];
+        String str = null;
+        try {
+            j = 18 / i;
+            System.out.println(str.length());
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);// this will throw an out of bound exception
+        }
+         catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero");
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Stay in you limit");
+        }
+        catch(java.lang.Exception e){
+            System.out.println("Something went wrong"+ e);//null pointer exception cuz str is null 
+        }
+        System.out.println(j);
+        System.out.println("sorry");
+    }
+}
+//NOTE:- Exception is the parent of all the exception and it is alway mentioned at the bottom of all the exception
+//because it can handle all type of exception so the program wont reach the other catch blocks and execute them cuz they will be already handled.
