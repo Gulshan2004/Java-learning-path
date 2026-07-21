c226f73ebcc63ae4e075e0e5c84d27c92479659a
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-21 10:50:24 +0530

    Ducking Exception using throws

--- Java-Fundamentals/Advance_Java/Exception-Handling/Throws_keyword.java
+++ Java-Fundamentals/Advance_Java/Exception-Handling/Throws_keyword.java
@@ -1,11 +1,8 @@
 class E {
-    public void show() throws ClassNotFoundException {// throws says now i wont handle the exception it will be handles by someone else which is the main method
-        // try {
-            Class.forName("Calc");// try is not able to find the class Calc so it sents the exception to the catch block
-        // } 
-        // catch (ClassNotFoundException e) {
-        //     System.out.println("Not Able to find the class ");
-        // }
+    public void show() throws ClassNotFoundException {// throws says now I wont handle the exception it will be handled by the method who is calling it which is main in this case this is called ducking .
+          
+        Class.forName("Calc");//here try is not able to find the class Calc so it sents the exception 
+       
     }
 }
 
@@ -18,7 +15,8 @@ public class Throws_keyword {
         try {
             obj.show();
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            e.printStackTrace(); //it prints the entire stack like which method is calling whome and we can track the problem
         }
     }
 }
+//Never write throws with the main method cuz main is handled my jvm and jvm will stop the execution if it handles the exception
