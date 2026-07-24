589f40ac3a3656fa3e9402e9aec68ac32c3fd82a
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-24 13:24:00 +0530

    Working with ArrayList

--- Java-Fundamentals/Advance_Java/Collection/Array_List.java
+++ Java-Fundamentals/Advance_Java/Collection/Array_List.java
@@ -4,26 +4,26 @@ import java.util.List;
 
 public class Array_List {
     public static void main(String args[]) {
-        List<Integer> nums = new ArrayList<Integer>();
-        nums.add(6);//These are not Integers these are objectsthere for to work with integers DataType We use 
-                      // <> brackets inside which we define the class of Data type we want to work with 
+        List<Integer> nums = new ArrayList<Integer>();//Array LIst is a class which implements list which supports indexing so we can use it when we are working with index values
+        nums.add(6);//These are not Integers these are objects therefor to work with integers DataType We use 
+                      // <> brackets inside which we define the class of Data type we want to work with and it is useful in removing bugs as well       
         nums.add(5);
         nums.add(8);
         nums.add(2);
 
-        System.out.println(nums.get(2));
+        System.out.println(nums.get(2)); // OUTPUT:-8
 
-        // for (int n : nums) {
+        // for (int n : nums) {   //The Collection Interface dont have indexing so we used loop here
         //     System.out.println(n);
         // }
     }
 }
 /*
 NOTE:-
+ByDefault the Collection  works with objects
+
 The collection interface belong to a java.util interface so we need to import
 it
-  
-ByDefault the Collection  works with objects
 
 Collection<Integer> nums = new ArrayList<Integer>(); we can access the index from the collection but he list inteface have the method for getting 
 the index so we used List Instead
