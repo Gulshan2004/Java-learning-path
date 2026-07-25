96560847eb4d7487a6c0a6c5a0122673d5fa2e79
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-07-25 22:51:10 +0530

    Understandiing Comparator and Comparable

--- Java-Fundamentals/Advance_Java/Collection/ComparatorVsComparable.java
+++ Java-Fundamentals/Advance_Java/Collection/ComparatorVsComparable.java
@@ -1,5 +1,7 @@
+package Collection;
+
+import java.util.Collection;
 import java.util.ArrayList;
-// import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
