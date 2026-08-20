4e51b600198d6b9807727912accca6b0ca0a5a5d
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-08-21 00:09:24 +0530

    Exploring Spring Framework

--- Spring1/src/main/java/com/gulshan/Alien.java
+++ Spring1/src/main/java/com/gulshan/Alien.java
@@ -2,9 +2,20 @@ package com.gulshan;
 
 public class Alien {
 
+    private int age;
     public Alien(){
         System.out.println("Object Created");
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setter called ");
+        this.age = age;
+    }
+
     public void code(){
         System.out.println("Coding");
     }
--- Spring1/src/main/java/com/gulshan/App.java
+++ Spring1/src/main/java/com/gulshan/App.java
@@ -11,12 +11,13 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //This will create the spring container and the configuration for that container is "spring.xml" it will create all the objects mentioned with the bean tag in the spring.xml .
         //Alien obj = new Alien();
         Alien obj1 = (Alien) context.getBean("alien1"); //here we are just calling the object
-        obj1.code(); //and working with the object in the container
+//        obj1.setAge(21);
+        System.out.println(obj1.getAge());
+        obj1.code();
+//
+//        Alien obj2 = (Alien) context.getBean("alien1"); //here we are just calling the object
+//        System.out.println(obj2.age);
 
-        Alien obj2 = (Alien) context.getBean("alien1"); //here we are just calling the object
-        obj2.code();
+//        obj2.code();
     }
 }
-/*
-* NOTE:-
-* the more the no. of bean created in the Spring.xml for the same class the more object will be crated for that class */
