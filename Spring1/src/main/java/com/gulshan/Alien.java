f8d6d254dcbcbdfeb42b8249a65d87322f9a8635
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-08-21 00:35:03 +0530

    Exploring Spring Framework

--- Spring1/src/main/java/com/gulshan/Alien.java
+++ Spring1/src/main/java/com/gulshan/Alien.java
@@ -3,6 +3,7 @@ package com.gulshan;
 public class Alien {
 
     private int age;
+    private Laptop lap;
     public Alien(){
         System.out.println("Object Created");
     }
@@ -16,7 +17,16 @@ public class Alien {
         this.age = age;
     }
 
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
         System.out.println("Coding");
+        lap.compile();
     }
 }
