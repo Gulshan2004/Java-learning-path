10406b53b06351f8cf9fbec64d95af12a75702f4
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-08-23 21:27:13 +0530

    Exploring Spring Framework

--- Spring1/src/main/java/com/gulshan/Alien.java
+++ Spring1/src/main/java/com/gulshan/Alien.java
@@ -4,36 +4,36 @@ public class Alien {
 
     private int age;
     private int salary;
-    private Laptop lap;
+    private Computer com;
     public Alien(){
-        System.out.println("Object Created");
+//        System.out.println("Object Created");
     }
 
-    public Alien(int age,Laptop lap) {
-        System.out.println("para constructor Called");
-        this.age = age;
-        this.lap =lap;
-    }
+//    public Alien(int age, Computer com) {
+//        System.out.println("para constructor Called");
+//        this.age = age;
+//        this.lap =lap;
+//    }
 
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
-        System.out.println("setter called ");
+//        System.out.println("setter called ");
         this.age = age;
     }
 
-    public Laptop getLap() {
-        return lap;
+    public Computer getCom() {
+        return com;
     }
 
-    public void setLap(Laptop lap) {
-        this.lap = lap;
+    public void setCom(Computer com) {
+        this.com = com;
     }
 
     public void code(){
         System.out.println("Coding");
-        lap.compile();
+        com.compile();
     }
 }
--- Spring1/src/main/java/com/gulshan/Laptop.java
+++ Spring1/src/main/java/com/gulshan/Laptop.java
@@ -1,10 +1,11 @@
 package com.gulshan;
 
-public class Laptop {
-    public Laptop(){
-        System.out.println("Laptop object created ");
-    }
+public class Laptop implements Computer {
+//    public Laptop(){
+//        System.out.println("Laptop object created ");
+//    }
+    @Override
     public void compile(){
-        System.out.println("compiling");
+        System.out.println("compiling using laptop");
     }
 }
