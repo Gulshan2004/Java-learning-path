e22bdf752066ba7aa9f54243fa567745a992ad4a
Gulshan2004 <gulshankumar1923070@gmail.com>
2026-08-24 22:29:40 +0530

    Exploring Spring Framework

--- Spring1/src/main/java/com/gulshan/Alien.java
+++ Spring1/src/main/java/com/gulshan/Alien.java
@@ -6,7 +6,7 @@ public class Alien {
     private int salary;
     private Computer com;
     public Alien(){
-//        System.out.println("Object Created");
+        System.out.println("Alien Object Created");
     }
 
 //    public Alien(int age, Computer com) {
--- Spring1/src/main/java/com/gulshan/Laptop.java
+++ Spring1/src/main/java/com/gulshan/Laptop.java
@@ -1,9 +1,9 @@
 package com.gulshan;
 
 public class Laptop implements Computer {
-//    public Laptop(){
-//        System.out.println("Laptop object created ");
-//    }
+    public Laptop(){
+        System.out.println("Laptop object created ");
+    }
     @Override
     public void compile(){
         System.out.println("compiling using laptop");
